c4b07c5ac28fb8ae1f379530123d3a477df7be29
Ice Chiang <icespeech@gmail.com>
2015-06-16 16:29:15 +0800

    add some test cases

--- leetcode/Multiply Strings/Multiply Strings/main.cpp
+++ leetcode/Multiply Strings/Multiply Strings/main.cpp
@@ -4,11 +4,26 @@
 int main(int argc, char* argv[])
 {
 	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	RUN_ALL_TESTS();
+
+	getchar();
+	return 0;
 }
 
 TEST(BasicTest, MultiplyShortPositiveNumbers)
 {
 	Solution solution;
 	EXPECT_STREQ("1", solution.multiply("1", "1").c_str());
+}
+
+TEST(BasicTest, MultiplyLongPositiveNumberWithShort)
+{
+    Solution solution;
+    EXPECT_STREQ("135802468864197531", solution.multiply("12345678987654321", "11").c_str());
+}
+
+TEST(BasicTest, MultiplyLongPositiveNumbers)
+{
+    Solution solution;
+    EXPECT_STREQ("1219326320073159566072245112635269", solution.multiply("12345678987654321", "98765432123456789").c_str());
 }
