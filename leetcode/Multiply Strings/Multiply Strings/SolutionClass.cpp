4e28d1677fcb5b1c84e1dde507a52e2f37eb768a
Ice Chiang <icespeech@gmail.com>
2015-06-16 18:09:14 +0800

    the final version with unit test codes

--- leetcode/Multiply Strings/Multiply Strings/SolutionClass.cpp
+++ leetcode/Multiply Strings/Multiply Strings/SolutionClass.cpp
@@ -1,6 +1,76 @@
 #include "SolutionClass.h"
+#include <vector>
+using std::vector;
+
+void a2iForVector(vector<char>& aVec)
+{
+	for (auto it = aVec.begin(); it != aVec.end(); ++it)
+		*it -= '0';
+}
+void i2aForVector(vector<char>& iVec)
+{
+	for (auto it = iVec.begin(); it != iVec.end(); ++it)
+		*it += '0';
+}
+
+void multiplyByDigit(vector<char>& bigNumber, const vector<char>& num1, char multiplier, int digit)
+{
+	int tempDigit;
+	for (int i = num1.size()-1, inserter = digit; i >= 0; --i, ++inserter)
+	{
+		tempDigit = multiplier * num1[i];
+
+		// when the vector is out of digits to be added
+		if (inserter == bigNumber.size())
+		{
+			bigNumber.push_back(tempDigit);
+		}
+		else
+		{
+			bigNumber[inserter] += tempDigit;
+		}
+	}
+
+	for (int i = 0; i < bigNumber.size(); ++i)
+	{
+		if (bigNumber[i] >= 10)
+		{
+			if (i == bigNumber.size() - 1)
+			{
+				bigNumber.push_back(bigNumber[i] / 10);
+			}
+			else
+			{
+				bigNumber[i + 1] += bigNumber[i] / 10;
+			}
+
+			bigNumber[i] %= 10;
+		}
+	}
+}
 
 string Solution::multiply(string num1, string num2)
 {
-    return "";
+	// if num1 or num2 is zero, the result will be zero
+	if ((num1.size() == 1 && num1[0] == '0') ||
+		(num2.size() == 1 && num2[0] == '0'))
+	{
+		return "0";
+	}
+
+	vector<char> num1Vec(num1.begin(), num1.end());
+	vector<char> num2Vec(num2.begin(), num2.end());
+	a2iForVector(num1Vec);
+	a2iForVector(num2Vec);
+
+	vector<char> bigNumber(num1.size(), 0);
+
+	for (int digit = 0, i = num2.size() - 1; i >= 0; ++digit, --i)
+	{
+		multiplyByDigit(bigNumber, num1Vec, num2Vec[i], digit);
+	}
+	
+	i2aForVector(bigNumber);
+
+	return string(bigNumber.rbegin(), bigNumber.rend());
 }
--- leetcode/Multiply Strings/Multiply Strings/main.cpp
+++ leetcode/Multiply Strings/Multiply Strings/main.cpp
@@ -10,20 +10,50 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-TEST(BasicTest, MultiplyShortPositiveNumbers)
+TEST(BasicTest, SimpleMultiply)
 {
 	Solution solution;
 	EXPECT_STREQ("1", solution.multiply("1", "1").c_str());
 }
 
-TEST(BasicTest, MultiplyLongPositiveNumberWithShort)
+TEST(BasicTest, MultiplyEasyNumbers)
+{
+	Solution solution;
+	EXPECT_STREQ("56088", solution.multiply("123", "456").c_str());
+}
+
+TEST(BasicTest, MultiplyEasyNumbersForCarries)
+{
+	Solution solution;
+	EXPECT_STREQ("516006", solution.multiply("789", "654").c_str());
+}
+
+TEST(BigNumberTest, MultiplyLongPositiveNumberWithShort)
 {
     Solution solution;
     EXPECT_STREQ("135802468864197531", solution.multiply("12345678987654321", "11").c_str());
 }
 
-TEST(BasicTest, MultiplyLongPositiveNumbers)
+TEST(BigNumberTest, MultiplyLongPositiveNumbers)
 {
     Solution solution;
     EXPECT_STREQ("1219326320073159566072245112635269", solution.multiply("12345678987654321", "98765432123456789").c_str());
+}
+
+TEST(ZeroTest, MultiplyLeftZero)
+{
+	Solution solution;
+	EXPECT_STREQ("0", solution.multiply("0", "98765432123456789").c_str());
+}
+
+TEST(ZeroTest, MultiplyRightZero)
+{
+	Solution solution;
+	EXPECT_STREQ("0", solution.multiply("12345678987654321", "0").c_str());
+}
+
+TEST(ZeroTest, MultiplyAllZero)
+{
+	Solution solution;
+	EXPECT_STREQ("0", solution.multiply("0", "0").c_str());
 }
