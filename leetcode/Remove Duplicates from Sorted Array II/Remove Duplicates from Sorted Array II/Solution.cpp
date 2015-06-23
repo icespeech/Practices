7f5e03bee89a5cc0203d471d00197caac369bee4
Ice Chiang <icespeech@gmail.com>
2015-06-23 11:13:54 +0800

    Simply finished

--- leetcode/Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II/Solution.cpp
+++ leetcode/Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II/Solution.cpp
@@ -2,5 +2,32 @@
 
 int Solution::removeDuplicates(std::vector<int>& nums)
 {
+	if (nums.size() < 3)
+	{
+		return nums.size();
+	}
 
+	bool isDuplicated = false;
+	unsigned int insertingIndex = 1;
+
+	for (auto it = nums.begin() + 1; it != nums.end(); ++it)
+	{
+		if (nums[insertingIndex - 1] != *it)
+		{
+			nums[insertingIndex++] = *it;
+			isDuplicated = false;
+		}
+		else
+		{
+			if (!isDuplicated)
+			{
+				nums[insertingIndex++] = *it;
+				isDuplicated = true;
+			}
+		}
+	}
+
+	nums.erase(nums.begin()+insertingIndex, nums.end());
+
+	return nums.size();
 }
