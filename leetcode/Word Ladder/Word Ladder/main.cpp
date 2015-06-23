5d7e68836ea22d798e22b18c64217ac7aeb3804e
Ice Chiang <icespeech@gmail.com>
2015-06-23 11:27:54 +0800

    Modified the main.cpp to use unit tests

--- leetcode/Word Ladder/Word Ladder/main.cpp
+++ leetcode/Word Ladder/Word Ladder/main.cpp
@@ -1,20 +1,43 @@
-#include <iostream>
 #include "Solution.h"
-using std::cout;
+#include "gtest\gtest.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    string start = "talk";
-    string end = "tail";
-    //char* dictInCStr[] = { "hot", "dot", "dog", "lot", "log" };
-    char* dictInCStr[] = { "talk","tons","fall","tail","gale","hall","negs" };
-    std::unordered_set<string> dict(&dictInCStr[0], &dictInCStr[7]);
-    Solution solution;
-    
-    cout << solution.ladderLength(start, end, dict);
+	::testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
 
-    int i;
-    std::cin >> i; 
+	// pause to show the results
+	getchar();
+	return 0;
+}
 
-    return 0;
+TEST(EmptyInputTest, EmptyInputTest)
+{
+	Solution sol;
+	std::unordered_set<string> dict;
+	EXPECT_EQ(0, sol.ladderLength("hot", "dog", dict));
+}
+
+TEST(NoPathTest, NoPathTest)
+{
+	Solution sol;
+	std::unordered_set<string> dict = { "hit" };
+
+	EXPECT_EQ(0, sol.ladderLength("hot", "dog", dict));
+}
+
+TEST(SimplePathTest, SimplePathTest)
+{
+	Solution sol;
+	std::unordered_set<string> dict = { "hot", "dog", "dot" };
+
+	EXPECT_EQ(3, sol.ladderLength("hot", "dog", dict));
+}
+
+TEST(LeetCodeExampleTest, LeetCodeExampleTest)
+{
+	Solution sol;
+	std::unordered_set<string> dict = { "hot", "dot", "dog", "lot", "log" };
+
+	EXPECT_EQ(5, sol.ladderLength("hit", "cog", dict));
 }
