85a7b1b769f1220585a325ac4dfd4b5498952eb6
Ice Chiang <icespeech@gmail.com>
2015-06-24 16:25:04 +0800

    Added a new project

---
+++ leetcode/Word Ladder II/Word Ladder II/Solution.cpp
@@ -0,0 +1,8 @@
+#include "Solution.h"
+using std::vector;
+using std::string;
+using std::unordered_set;
+
+vector<vector<string>> Solution::findLadders(string start, string end, unordered_set<string> &dict)
+{
+}
---
+++ leetcode/Word Ladder II/Word Ladder II/Solution.h
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+class Solution
+{
+public:
+    std::vector<std::vector<std::string>> findLadders(std::string start, std::string end, std::unordered_set<std::string> &dict);
+};
