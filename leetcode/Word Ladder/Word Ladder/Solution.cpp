faf98aa36d55f4db9fdfd68d0dc3253357d0a8c6
Ice Chiang <icespeech@gmail.com>
2015-07-21 18:08:42 +0800

    Re-wrote with a total different thought. Significantly imporved the efficiency

--- leetcode/Word Ladder/Word Ladder/Solution.cpp
+++ leetcode/Word Ladder/Word Ladder/Solution.cpp
@@ -1,57 +1,95 @@
 #include "Solution.h"
 using std::string;
 
+bool stringAdj(const string& a, const string& b)
+{
+    for (int i = 0, differ = 0; i < a.size(); ++i)
+        if (a[i] != b[i] && ++differ > 1) return false;
+    
+    return true;
+}
+
+bool setAdj(const string& str, const std::unordered_set<string> st)
+{
+    for (auto s : st)
+        if (stringAdj(s, str)) return true;
+    
+    return false;
+}
+
 int Solution::ladderLength(string beginWord, string endWord, std::unordered_set<string>& wordDict)
 {
-    if (beginWord.compare(endWord) == 0)
+    if (beginWord.size() == 0 || endWord.size() == 0 || beginWord.size() != endWord.size()) return 0;
+    if (beginWord.compare(endWord) == 0) return 1;
+    if (stringAdj(beginWord, endWord)) return 2;
+
+    std::unordered_set<string> layerSetFromStart;
+    std::unordered_set<string> layerSetFromEnd;
+    std::unordered_set<string> tmpLayerSetFromStart;
+    std::unordered_set<string> tmpLayerSetFromEnd;
+
+    layerSetFromStart.insert(beginWord);
+    layerSetFromEnd.insert(endWord);
+
+    int startDepth = 1, endDepth = 1;
+
+    while (layerSetFromStart.size() > 0 || layerSetFromEnd.size() > 0)
     {
-        return 1;
-    }
-
-    std::unordered_set<Vertex*> vertices;
-
-    Vertex* start = new Vertex(beginWord, &mWaitingLineSet, &mWaitingLine);
-    vertices.insert(start);
-
-    Vertex* end = new Vertex(endWord, &mWaitingLineSet, &mWaitingLine);
-    vertices.insert(end);
-
-    for (auto str: wordDict)
-    {
-        if (str.compare(beginWord) == 0 || str.compare(endWord) == 0)
-            continue;
-        Vertex* v = new Vertex(str, &mWaitingLineSet, &mWaitingLine);
-
-        for (auto vt : vertices)
+        /*
+        * start side
+        */
+        for (auto curStart : layerSetFromStart)
         {
-            if (v->adjacentTo(vt))
+            for (int i = 0; i < curStart.size(); ++i)
             {
-                v->addAdjacent(vt);
-                vt->addAdjacent(v);
+                string tmpS = curStart;
+                for (char c = 'a'; c <= 'z'; ++c)
+                {
+                    if (c == curStart[i]) continue;
+
+                    tmpS[i] = c;
+                    // there is this node, and this node is in the leaf layer of the end side => found
+                    if (layerSetFromEnd.find(tmpS) != layerSetFromEnd.end())
+                        return startDepth + endDepth;
+
+                    // there is this node in the dict
+                    if (wordDict.find(tmpS) != wordDict.end())
+                    {
+                        tmpLayerSetFromStart.insert(tmpS);
+                    }
+                }
             }
         }
-        vertices.insert(v);
-    }
+        startDepth++;
+        layerSetFromStart = tmpLayerSetFromStart;
+        tmpLayerSetFromStart.clear();
 
-    mWaitingLine.push(std::make_pair(start, 2));
-    
-    while (mWaitingLine.size() > 0 && mWaitingLine.front().second <= vertices.size())
-    {
-        if (mWaitingLine.front().first->search(mWaitingLine.front().second, endWord))
+        /*
+        * end side
+        */
+        for (auto curEnd : layerSetFromEnd)
         {
-            return mWaitingLine.front().second;
-        }
-        else
-        {
-            mWaitingLine.pop();
-        }
-    }
+            for (int i = 0; i < curEnd.size(); ++i)
+            {
+                string tmpE = curEnd;
+                for (char c = 'a'; c <= 'z'; ++c)
+                {
+                    if (c == curEnd[i]) continue;
 
-    // over
-    for (auto v: vertices)
-    {
-        delete v;
-    }
+                    tmpE[i] = c;
+                    if (layerSetFromStart.find(tmpE) != layerSetFromStart.end())
+                        return startDepth + endDepth;
 
+                    // there is this node in the dict
+                    if (wordDict.find(tmpE) != wordDict.end())
+                        tmpLayerSetFromEnd.insert(tmpE);
+                }
+            } // end for loop of every character of the word
+        }
+
+        endDepth++;
+        layerSetFromEnd = tmpLayerSetFromEnd;
+        tmpLayerSetFromEnd.clear();
+    }
     return 0;
 }
--- leetcode/Word Ladder/Word Ladder/Solution.h
+++ leetcode/Word Ladder/Word Ladder/Solution.h
@@ -1,11 +1,8 @@
-#include "Vertex.h"
+#include <string>
+#include <unordered_set>
 
 class Solution
 {
 public:
     int ladderLength(std::string beginWord, std::string endWord, std::unordered_set<std::string>& wordDict);
-
-private:
-    std::set<Vertex*> mWaitingLineSet;
-    std::queue<std::pair<Vertex*, unsigned int>> mWaitingLine;
 };
