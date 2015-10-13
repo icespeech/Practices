1411d73f80c79a6b8d6c762f339fa3a8f358864f
Ice Chiang <icespeech@gmail.com>
2015-10-13 11:32:17 +0800

    modified the Solution.cpp to include the .h file

--- leetcode/Largest Rectangle in Histogram/Largest Rectangle in Histogram/Solution.cpp
+++ leetcode/Largest Rectangle in Histogram/Largest Rectangle in Histogram/Solution.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+#include "Solution.h"
 #include <stack>
 #define h first
 #define pos second
@@ -7,43 +7,39 @@ using std::vector;
 using std::stack;
 using std::pair;
 
-class Solution
+int Solution::largestRectangleArea(vector<int>& height)
 {
-public:
-    int largestRectangleArea(vector<int>& height)
+    if (height.size() == 0) return 0;
+    if (height.size() == 1) return height[0];
+
+    // add a stop item to force the tail items to calculate
+    height.push_back(-1);
+
+    size_t maxArea = 0;
+    stack<pair<int, size_t>> heightStack;
+
+    heightStack.push(std::make_pair(height[0], 0));
+    for (int i = 1; i < height.size(); ++i)
     {
-        if (height.size() == 0) return 0;
-        if (height.size() == 1) return height[0];
-
-        // add a stop item to force the tail items to calculate
-        height.push_back(-1);
-
-        size_t maxArea = 0;
-        stack<pair<int, size_t>> heightStack;
-
-        heightStack.push(std::make_pair(height[0], 0));
-        for (int i = 1; i < height.size(); ++i)
+        if (height[i] >= heightStack.top().h)
         {
-            if (height[i] >= heightStack.top().h)
-            {
-                heightStack.push(std::make_pair(height[i], i));
-            }
-            else
-            {
-                // the equal position of this histogram bar
-                int equalPos = i;
-                while (!heightStack.empty() && height[i] < heightStack.top().h)
-                {
-                    equalPos = heightStack.top().pos;
-                    size_t tmpArea = heightStack.top().h * (i - heightStack.top().pos);
-                    if (tmpArea > maxArea)
-                        maxArea = tmpArea;
-                    heightStack.pop();
-                }
-                heightStack.push(std::make_pair(height[i], equalPos));
-            }
+            heightStack.push(std::make_pair(height[i], i));
         }
-
-        return maxArea;
+        else
+        {
+            // the equal position of this histogram bar
+            int equalPos = i;
+            while (!heightStack.empty() && height[i] < heightStack.top().h)
+            {
+                equalPos = heightStack.top().pos;
+                size_t tmpArea = heightStack.top().h * (i - heightStack.top().pos);
+                if (tmpArea > maxArea)
+                    maxArea = tmpArea;
+                heightStack.pop();
+            }
+            heightStack.push(std::make_pair(height[i], equalPos));
+        }
     }
-};
+
+    return maxArea;
+}
